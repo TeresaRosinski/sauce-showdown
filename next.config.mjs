da415cba2067b85f89266be11ecf9d751365cb21
Rosinski, Teresa <Teresa.Rosinski@epsilon.com>
2025-08-27 17:08:17 -0500

    feat: configure for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  output: 'export',
+  trailingSlash: true,
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/sauce-showdown' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/sauce-showdown' : '',
 }
 
 export default nextConfig
