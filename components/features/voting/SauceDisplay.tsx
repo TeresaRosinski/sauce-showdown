8775cf0c2863ae7e7731585dee2daf1c10fe458b
Rosinski, Teresa <Teresa.Rosinski@epsilon.com>
2025-08-28 21:21:54 -0500

    feat: more adjustments

--- components/features/voting/SauceDisplay.tsx
+++ components/features/voting/SauceDisplay.tsx
@@ -31,19 +31,18 @@ export function SauceDisplay({ sauce, side, isSelected, onVote, hasVoted = false
     backgroundColor: 'white',
     borderRadius: '12px',
     border: `3px solid ${getBorderColor()}`,
-    height: '115px',
+    height: '100px',
     //padding: '6px',
     display: 'flex',
-    alignItems: 'flex-end',
-    //justifyContent: 'space-evenly',
+    alignItems: 'center',
+    justifyContent: 'center',
     gap: '8px',
     opacity: hasVoted && !isSelected ? 0.6 : 1,
     transform: isSelected ? 'scale(1.02)' : 'scale(1)',
     transition: 'all 0.3s ease',
     cursor: hasVoted ? 'not-allowed' : 'pointer',
     boxShadow: getShadowColor(),
-    marginLeft: '10px',
-    margin: "10px 10px 10px 10px",
+    margin: side === "left" ? "0px 10px 20px 10px" : "20px 10px 0px 10px",
     // Safari-specific properties using string indexing
     ...({
       WebkitTransform: isSelected ? 'scale(1.02)' : 'scale(1)',
@@ -67,15 +66,8 @@ export function SauceDisplay({ sauce, side, isSelected, onVote, hasVoted = false
 
   return (
     <div style={containerStyle} onClick={handleClick}  className={`bg-white rounded ${hasVoted ? 'cursor-not-allowed opacity-60' : 'cursor-pointer'}`}>
-      
-      <Image
-        src={sauce.personImageUrl}
-        alt="Influencer"
-        width={60}
-        height={60}
-        className={`${  side === "left" ? 'border-red-400' : 'border-yellow-400'}`}
-        style={{ objectFit: 'contain', width: '110px', height: '100px' }}
-      />
+
+
 
       {/* Text Info Section */}
       <div style={{
@@ -83,14 +75,15 @@ export function SauceDisplay({ sauce, side, isSelected, onVote, hasVoted = false
         }}>
 
           <div>
-            <Image
-                  src={sauce.imageUrl}
-                  alt="Influencer"
-                  width={40}
-                  height={40}
-                  className=""
-                  style={{ objectFit: 'cover', marginBottom: '5px' }}
-            />
+          <Image
+            src={sauce.personImageUrl}
+            alt="Influencer"
+            width={60}
+            height={60}
+            className={`${  side === "left" ? 'border-red-400' : 'border-yellow-400'}`}
+            style={{ objectFit: 'contain', width: '45px', height: '45px' ,borderRadius: '50%' }}
+          />
+
           </div>
     
         <div> 
@@ -106,7 +99,7 @@ export function SauceDisplay({ sauce, side, isSelected, onVote, hasVoted = false
       </div>
        
       </div>
-      
+
     </div>
   )
 }
